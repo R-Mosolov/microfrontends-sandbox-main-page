3c2c7ee84eb7755dcccdfe981addb939491832b2
R-Mosolov <R.V.Mosolov@ya.ru>
2023-01-25 12:12:47 +0300

    Rewrite logic to run SPA by using Webpack

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import * as React from "react";
 import {
   createBrowserRouter,
   RouterProvider,
+  Link
 } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <h1>Main Page</h1>,
+      element: (
+        <>
+          <h1>Main Page</h1>
+          <Link to="/second-page">
+            Go to Second Page
+          </Link>
+        </>
+      ),
     },
     {
       path: "/second-page",
